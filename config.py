3dacacc8d6600385b29e36e11e449878695c3dc2
Mehinator <85841029+Mehinator@users.noreply.github.com>
2023-05-21 18:50:43 +0330

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hell")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
@@ -58,7 +58,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bott!"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
